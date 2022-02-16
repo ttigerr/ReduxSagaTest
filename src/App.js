b1ee48d0d18281b7186c56c790c1ed8ee4264c0a
Tiger <tiger.chalernphon15@gmail.com>
2022-02-17 00:31:59 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { createStore,  applyMiddleware} from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { helloSaga } from './components/saga';
 import './App.css';
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(helloSaga)
+
+const action = type => store.dispatch({type})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello This is my first Redux saga tutorial</p>
       </header>
     </div>
   );
